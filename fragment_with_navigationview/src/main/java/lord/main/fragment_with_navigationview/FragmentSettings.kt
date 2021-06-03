72e9ff82c90b2a6f01a1a545708ceee5ee545a03
dan9112_work_KamAZ <bessonovdanil9112@mail.ru>
2021-06-03 16:08:35 +0300

    upgrade
    Заменено использование перезаписи устаревшей функции
    onActivityCreated во фрагментах на одноимённую функцию с
    использованием наблюдения за жизненным циклом активности. Всем
    фрагментам добавлено использование интерфейса наблюдателя жизненного
    цикла

--- fragment_with_navigationview/src/main/java/lord/main/fragment_with_navigationview/FragmentSettings.kt
+++ fragment_with_navigationview/src/main/java/lord/main/fragment_with_navigationview/FragmentSettings.kt
@@ -1,12 +1,10 @@
 package lord.main.fragment_with_navigationview
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
-import lord.main.fragment_with_navigationview.databinding.FragmentSettingsBinding
+import androidx.fragment.app.Fragment
 
 class FragmentSettings : Fragment() {
     override fun onCreateView(
